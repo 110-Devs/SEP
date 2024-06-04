160a2271adbdba8dea8a385dbfe31853919d1857
rubeln <rubenseitz.q@gmail.com>
2024-06-04 17:01:34 +0200

    domJSON so einfach wie möglich vereinfachen

--- packages/lab/ai/src/lib/components/ChatInputField.tsx
+++ packages/lab/ai/src/lib/components/ChatInputField.tsx
@@ -2,7 +2,7 @@
 import SendIcon from '@mui/icons-material/Send';
 import Button from '@mui/material/Button';
 import axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import environment from '../environments/environment';
 import { request } from '../environments/modelConfig';
 import { InputContainer, InputField } from '../styles/ChatInputField.styles';
@@ -42,33 +42,38 @@ const ChatInputField = () => {
     console.log('Converting successful!');
 
     //Assigning a key to every class and selector in two different Maps
-    let classMap = new Map<string, string>();
-    let selectorMap = new Map<string, string>();
     const classNameGenerator = new ClassNameGenerator();
-    updateClassNames(codyJSON, classMap, selectorMap, classNameGenerator);
+    const newNode: any = updateClassNames(codyJSON, classNameGenerator);
+    const classArr = newNode.classMap;
+    const selectorArr = newNode.selectorMap;
 
     //Testing
     console.log(codyJSON);
-    //console.log(classMap);
-    console.log(selectorMap);
+    console.log(classArr);
+    console.log(selectorArr);
 
     //Converting everything to String for the prompt
     const jsonString = JSON.stringify(codyJSON);
     let classString = '';
     let selectorString = '';
 
-   /* //WIP Berke
-    for (const i in classMap) {
-      classString = i + '=>' + classMap[i] + '\n'
+    //WIP Berke
+    for (const i in classArr) {
+      classString = i + '=>' + classArr[i] + '\n'
     }
 
-    for (const j in selectorMap) {
-      selectorString = j + '=>' + selectorMap[j] + '\n'
+    for (const j in selectorArr) {
+      selectorString = j + '=>' + selectorArr[j] + '\n'
     }
-    */
+    
+    //Testing
+    //console.log(classString);
+    //console.log(selectorString);
 
+    //User-Prompt
     console.log('Processing prompt:', prompt);
 
+    //HIER WICHTIGE STRING KONKATENIERUNG
     let req = jsonString + '\n\n' + request({ prompt });
     const API_URL = `${environment.HOST}:${environment.PORT}${environment.ROUTES.SEND_PROMPT}`;
 
