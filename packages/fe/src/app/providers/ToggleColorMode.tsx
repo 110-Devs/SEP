e9a2a6f78adecac29f40e933adb979c5da6055a4
rubeln <104936556+rubeln@users.noreply.github.com>
2024-06-27 22:32:10 +0200

    Diverse Bugfixes und Verbesserungen am Chat und an (#22)
    den Templates. Neue Themes hinzugefügt.
    
    * ChatWindow: Nachrichten werden jetzt bei Schreibsimulation korrekt geschrieben ohne das Buchstaben verschluckt werden.
    * ChatWindow: Nachrichten können jetzt auch mit Enter abgesendet werden.
    * ChatWindow: Alternative Nachrichten für die KI zum schreiben hinzugefügt für bessere Chatinteraktion.
    * ChatWindow: Text des Chats jetzt auch bei Darkmode lesbar.
    * Templates: Neue Themes hinzugefügt.
    * Templates: Farbpalette als Vorschau hinzugefügt.
    * Templates: Buttons ändern jetzt Farbe mit Template Auswahl.
    *Weitere kleine Bugfixes und Verbesserungen.
    
    Co-authored-by: Luca <lucaahlf@gmx.de>
    Co-authored-by: 2211567 <2211567@stud.hs-mannheim.de>

--- packages/fe/src/app/providers/ToggleColorMode.tsx
+++ packages/fe/src/app/providers/ToggleColorMode.tsx
@@ -12,9 +12,17 @@ import {
   darkPurpleTheme,
   blackTheme,
   whiteTheme,
-} from '../../../../lab/fe/src/lib/components/template/CustomThemes'; // Adjust the import path as necessary
+  blueOceanTheme,
+  darkBlueOceanTheme,
+  coralReefTheme,
+  darkCoralReefTheme,
 
+} from '../../../../lab/fe/src/lib/components/template/CustomThemes';
+
+// Define the possible font types
 export type FontType = 'Roboto' | 'Montserrat' | 'Source Code Pro' | 'Ubuntu' | ' Dancing Script' | 'Kalam';
+
+// Define the possible theme types
 type ThemeType =
   | 'light'
   | 'dark'
@@ -25,8 +33,13 @@ type ThemeType =
   | 'purple'
   | 'darkPurple'
   | 'black'
-  | 'white';
+  | 'white'
+  | 'blueOcean'
+  | 'darkBlueOcean'
+  | 'coralReef'
+  | 'darkCoralReef';
 
+  // Define the structure of the ColorModeContext
 interface ColorModeContextProps {
   mode: ThemeType;
   font: FontType;
@@ -36,6 +49,7 @@ interface ColorModeContextProps {
   setFont: (font: FontType) => void;
 }
 
+// Create the ColorModeContext with default values
 export const ColorModeContext = createContext<ColorModeContextProps>({
   mode: 'light',
   font: 'Roboto',
@@ -45,6 +59,7 @@ export const ColorModeContext = createContext<ColorModeContextProps>({
   setFont: () => {},
 });
 
+// Helper function to merge typography with selected font family
 const mergeTypographyWithFontFamily = (baseTypography: any, fontFamily: string) => {
   return {
     ...baseTypography,
@@ -65,7 +80,9 @@ const mergeTypographyWithFontFamily = (baseTypography: any, fontFamily: string) 
   };
 };
 
+// Main component to toggle color modes and fonts
 const ToggleColorMode = ({ children }: { children: ReactNode }) => {
+  // States to manage the current theme and font
   const [mode, setMode] = useState<ThemeType>('light');
   const [font, setFont] = useState<FontType>('Roboto');
 
@@ -73,13 +90,14 @@ const ToggleColorMode = ({ children }: { children: ReactNode }) => {
     mode,
     font,
     toggleColorMode: () => {
+      // Toggle between light and dark mode
       setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
     },
     setTheme: (theme: ThemeType) => {
-      setMode(theme);
+      setMode(theme); // Set the selected theme
     },
     resetTheme: () => {
-      setMode('light');
+      setMode('light'); // Reset to light theme
     },
     setFont,
   }), [mode, font]);
@@ -87,6 +105,7 @@ const ToggleColorMode = ({ children }: { children: ReactNode }) => {
   const selectedTheme = useMemo(() => {
     let themeOptions;
 
+    // Choose the theme options based on the current mode
     switch (mode) {
       case 'dark':
         themeOptions = darkTheme;
@@ -115,11 +134,23 @@ const ToggleColorMode = ({ children }: { children: ReactNode }) => {
       case 'white':
         themeOptions = whiteTheme;
         break;
+      case 'blueOcean':
+        themeOptions = blueOceanTheme;
+        break;
+      case 'darkBlueOcean':
+        themeOptions = darkBlueOceanTheme;
+        break;
+      case 'coralReef':
+        themeOptions = coralReefTheme;
+        break;
+      case 'darkCoralReef':
+        themeOptions = darkCoralReefTheme;
+        break;
       case 'light':
       default:
         themeOptions = lightTheme;
     }
-
+    // Create the theme with merged typography and selected font
     const theme = createTheme({
       ...themeOptions,
       typography: mergeTypographyWithFontFamily(themeOptions.typography, font),
@@ -130,6 +161,7 @@ const ToggleColorMode = ({ children }: { children: ReactNode }) => {
   }, [mode, font]);
 
   return (
+    // Provide the color mode context to child components
     <ColorModeContext.Provider value={contextValue}>
       <MuiThemeProvider theme={selectedTheme}>
         <CssBaseline />
