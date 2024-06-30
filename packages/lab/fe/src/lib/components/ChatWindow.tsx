3ed6f2fb9ba0d8f0be3ad58ba95ed3a5e8f091d2
Nibelson <149329899+Nibelson@users.noreply.github.com>
2024-06-30 12:41:21 +0200

    Dev (#28)
    * Persistierung für Chatlog & Templates
    
    * Diverse Bugfixes bezüglich Templates und Chat
    
    * Cycle Funktion implmentiert
    
    ---------
    
    Co-authored-by: rubeln <104936556+rubeln@users.noreply.github.com>

--- packages/lab/fe/src/lib/components/ChatWindow.tsx
+++ packages/lab/fe/src/lib/components/ChatWindow.tsx
@@ -46,8 +46,8 @@ const ChatWindow: React.FC<ChatWindowProps> = ({ handleClose }) => {
   const inputField = useRef<HTMLInputElement>(null);
   const lastMessageRef = useRef<HTMLDivElement | null>(null);
   const hasTypedInitialMessage = useRef(false); // Add ref to track initial message
-  const route = useRouteStore((state) => state.currentRoute);
   const lastMessageWasGreeting = useRef(false);
+  const messageHistoryIndex = useRef<number>(-1);
 
   // Types of responses the chat can handle
   type ResponseType = 'greeting' | 'processing' | 'completed';
@@ -135,16 +135,48 @@ const ChatWindow: React.FC<ChatWindowProps> = ({ handleClose }) => {
     }
   }, [messages]);
 
+  // Focus on the input field when the chat window is opened
+  useEffect(() => {
+    if (inputField.current) {
+      inputField.current.focus();
+    }
+  }, []);
+
   // Handle input changes
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
+    if (event.target.value === '') {
+      messageHistoryIndex.current = -1; // Reset the message history index when input is cleared to reset cycling
+    }
   };
 
-  // Handle key down events for sending messages with Enter key
+  // Handle key down events for sending messages with Enter key and cycling through previous messages with Up arrow key
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>): void => {
     if (event.key === 'Enter' && !event.shiftKey && inputValue.trim() !== '') {
       event.preventDefault();
       handleSendMessage();
+    } else if (event.key === 'ArrowUp') {
+      handleCycleMessages('up');
+    } else if (event.key === 'ArrowDown') {
+      handleCycleMessages('down');
+    }
+  };
+
+  // Handle cycling through previous messages with Up and Down arrow keys
+  const handleCycleMessages = (direction: 'up' | 'down') => {
+    const userMessages = JSON.parse(localStorage.getItem('chatMessages') || '[]')
+      .filter((message: { content: string; isUser: boolean }) => message.isUser);
+
+    if (userMessages.length > 0) {
+      let newIndex = messageHistoryIndex.current;
+      if (direction === 'up') {
+        newIndex = newIndex < userMessages.length - 1 ? newIndex + 1 : newIndex;
+      } else {
+        newIndex = newIndex > 0 ? newIndex - 1 : -1;
+      }
+      const messageToDisplay = newIndex !== -1 ? userMessages[userMessages.length - 1 - newIndex].content : '';
+      setInputValue(messageToDisplay);
+      messageHistoryIndex.current = newIndex;
     }
   };
 
